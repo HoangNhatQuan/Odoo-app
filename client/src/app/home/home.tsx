9fad3801a83c6a63eb9b72bdf79e0705a0f19124
HoangNhatQuan <quanhn@metadatasolutions.vn>
2024-09-17 10:52:13 +0700

    chore: fix auth

--- client/src/app/home/home.tsx
+++ client/src/app/home/home.tsx
@@ -1,49 +1,11 @@
-import { useInfiniteQuery } from 'react-query'
-
-import { getVideos } from '@/apis/video/video.api'
 import Container from '@/components/container'
-import VideoList from './videos'
-import Notifications from './notifications'
-import LoadingPage from '@/components/loading'
 
 export default function Home() {
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isLoading,
-    isError,
-  } = useInfiniteQuery(
-    'videos',
-    ({ pageParam = 1 }) => getVideos({ page: pageParam, pageSize: 10 }),
-    {
-      getNextPageParam: (lastPage) => {
-        if (lastPage.currentPage < lastPage.totalPages) {
-          return lastPage.currentPage + 1
-        }
-        return undefined
-      },
-    },
-  )
-
-  const videos = data ? data.pages.flatMap((page) => page.items) : []
-
-  if (isLoading) return <LoadingPage msg="Loading..." />
-  if (isError) return <div className="m-auto">Error: unknown error</div>
-
   return (
     <Container bodyClassName="flex-col gap-4 py-4">
       <div className="flex flex-row justify-between items-center ">
         <h3 className="font-bold">New Feeds</h3>
-        <Notifications />
       </div>
-      <VideoList
-        videos={videos}
-        fetchNextPage={fetchNextPage}
-        hasNextPage={hasNextPage}
-        isFetchingNextPage={isFetchingNextPage}
-      />
     </Container>
   )
 }
--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import Layout from './layout'
-import SharingPage from './home/sharing'
 import RegisterPage from './auth/register'
 import LoginPage from './auth/login'
 import Home from './home/home'
@@ -14,7 +13,6 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<HomeLayout />}>
             <Route index element={<Home />} />
-            <Route index path="/sharing" element={<SharingPage />} />
           </Route>
           <Route index path="/register" element={<RegisterPage />} />
           <Route index path="/login" element={<LoginPage />} />
